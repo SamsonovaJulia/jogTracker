b41308eb231c877841469633deb670e735b60d98
Yuliya Samsonova <yuliya.samsonova@rs-components.com>
2021-04-12 00:08:38 +0300

    updated styles for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,46 @@
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link, useRouteMatch, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./Header.scss";
-import logo from "./logo.svg";
-import filter from "./filter.svg";
-import filterActive from "./filter-active.svg";
+import logo from "../../assets/logoHeader.svg";
+import filter from "../../assets/filter.svg";
+import filterActive from "../../assets/filter-active.svg";
 import { activateFilter } from "../../redux/actions/index";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 function Header() {
+  const location = useLocation();
   const dispatch = useDispatch();
   const isUsingFilter = useSelector(
     (state) => state.filterIsActive.filterIsActive
   );
+
+  const [IsChecked, setIsChecked] = useState(false);
   let match = useRouteMatch("/jogs");
   const toggleFilter = () => {
     dispatch(activateFilter(!isUsingFilter));
   };
 
-  return (
+  const showMenu = () => {
+    setIsChecked(!IsChecked);
+  };
+
+  useEffect(() => {
+    setIsChecked(false);
+  }, [location]);
+
+  return !IsChecked ? (
     <header>
       <Link to="/">
         <img src={logo} className="logo" />
       </Link>
       <div className="menuWrapper">
         <nav>
-          <Link className="one" to="/jogs">
-            JOGS
-          </Link>
-          <Link className="two" to="/info">
-            INFO
-          </Link>
-          <Link className="three" to="/contacts">
-            CONTACT US
-          </Link>
+          <div className="menuitems">
+            <Link to="/jogs">JOGS</Link>
+            <Link to="/info">INFO</Link>
+            <Link to="/contacts">CONTACT US</Link>
+          </div>
         </nav>
         {match && (
           <img
@@ -40,8 +49,18 @@ function Header() {
             onClick={toggleFilter}
           ></img>
         )}
+        {!IsChecked && (
+          <>
+            <label className="hamburger" for="hamburger">
+              &#9776;
+            </label>
+            <input type="checkbox" id="hamburger" onClick={showMenu} />
+          </>
+        )}
       </div>
     </header>
+  ) : (
+    <BurgerMenu onClose={showMenu} />
   );
 }
 
